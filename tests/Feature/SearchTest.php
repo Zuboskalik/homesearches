90cec0753ba0571191808ceb906ef64996ad5e48
Ivan Shorikov <shorikov@echo-company.ru>
2021-03-01 11:42:16 +0500

    typo fix

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -8,17 +8,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SearchTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testPageOpen()
-    {
-        $this->visit('/')
-             ->type('0', 'pricemin')
-             ->dontSee('No results found...');
-    }
     public function testPriceMinCorrect()
     {
         $this->visit('/')
